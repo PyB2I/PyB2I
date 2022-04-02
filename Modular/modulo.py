35aeff0a988eea0c0173db58d8d32868c1724163
Julio Tentor <jtentor@gmail.com>
2022-04-02 11:33:03 -0300

    4th meeting check 1.0

---
+++ Modular/modulo.py
@@ -0,0 +1,13 @@
+# comentario de una línea
+
+# para utilizar lo que está en este módulo se debe agregar 
+#import modulo
+
+def some_function() :
+    u"""Descripción de esta función"""
+    print("Ejecutando some_function() ...")
+    input("Pulse enter para regresar a la siguiente línea de código desde donde se invocó esta funcion")
+    return
+
+# declaro una variable que se podrá utilizar cuando se importe este módulo
+
